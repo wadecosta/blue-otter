1da6b1b1d71c0c2cba107d647a0ea9d57e79fe9d
wadecosta <wadeccosta@gmail.com>
2024-09-26 16:36:05 -0700

    Fixed the user add sticky modal.

--- app/static/js/addStickyModal.js
+++ app/static/js/addStickyModal.js
@@ -1,23 +1,74 @@
-document.addEventListener('DOCContentLoaded', (event) => {
-	let modal = document.getElementById("addStickyModal");
-	let span = document.querySelector(".closeAddSticky");
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Script loaded successfully");
 
-	let key = sessionStorage.getItem("key");
-        let iv = document.getElementById("iv").value;
+    let modal = document.getElementById("addStickyModal");
+    if (!modal) {
+        console.error("Modal element not found");
+        return;
+    }
 
-	document.querySelectorAdd('.open-add-sticky-button').forEach(button => {
-		button.onclick = function() {
-			modal.showModal();
-		}
+    let span = document.querySelector(".closeAddSticky");
+
+    let key = sessionStorage.getItem("key");
+    let iv = document.getElementById("iv").value;
+
+    console.log("Modal element:", modal);
+
+    document.querySelectorAll('.open-add-sticky-button').forEach(button => {
+        button.onclick = function() {
+            modal.showModal();
+        }
+    });
+
+    span.onclick = function() {
+        modal.close();
+    }
+
+    modal.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.close();
+        }
+    });
+
+    let form = modal.querySelector("form");
+    form.addEventListener("submit", function(event) {
+    	event.preventDefault();
+
+	let titleInput = modal.querySelector("#title");
+	let descriptionInput = modal.querySelector("#description");
+
+	let titleValue = titleInput.value;
+        let descriptionValue = descriptionInput.value;
+
+	console.log(titleValue);
+        console.log(descriptionValue);
+
+	let encryptedTitle = encryptText(titleValue, key, iv);
+	let encryptedDescription = encryptText(descriptionValue, key, iv);
+
+	console.log(encryptedTitle);
+	console.log(encryptedDescription);
+
+	const xhr = new XMLHttpRequest();
+	xhr.open("POST", "/addSticky");
+	xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+	const body = JSON.stringify({
+		title: encryptedTitle.toString(),
+		description: encryptedDescription.toString()
 	});
 
-	span.onclick = function() {
-		modal.close();
-	}
-
-	modal.addEventListener('click', function(event) {
-		if (event.target === modal) {
-			modal.close();
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			if (xhr.status === 200) {
+				window.location.reload();
+			} else {
+				console.error("Failed to add sticky:", xhr,status, xhr.statusText);
+			}
 		}
-	});
+	};
+
+	xhr.send(body);
+	modal.close();
+    });
 });
